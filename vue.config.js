4e55b1947bc4aa5e19f4010f5db54122e16a63d9
duoci <103027795+duoci@users.noreply.github.com>
2022-04-21 17:33:00 +0800

    music

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '' : '/',
+  // 公共路径
+  indexPath: 'index.html',
+  // 相对于打包路径index.html的路径
+  outputDir: process.env.outputDir || 'dist',
+  // 'dist', 生产环境构建文件的目录
+  assetsDir: 'static',
+  // 相对于outputDir的静态资源(js、css、img、fonts)目录
+
   configureWebpack: {
     resolve: {
       alias: {
